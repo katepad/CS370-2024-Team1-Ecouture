337e7819dd24b953d796da8cbcc15603bc0462f0
Kate <159409808+katepad@users.noreply.github.com>
2024-09-24 19:27:25 -0700

    Update Main.java

--- code/src/Main.java
+++ code/src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
     public static void main(String[] args){
 
-        MyJDBC.connectDatabase();
+        MyFrame myFrame = new MyFrame(); //create a Frame
+        MyJDBC.openConnection(); //connect to Local Database
+        Runtime.getRuntime().addShutdownHook(new Thread(MyJDBC::closeConnection)); //close DB connection before exiting
 
     }
 
