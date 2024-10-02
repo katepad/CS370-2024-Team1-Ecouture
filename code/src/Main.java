aebcacb77d4c9d2f8d3daf4b03dbe35a476457e5
Kate <159409808+katepad@users.noreply.github.com>
2024-10-02 01:00:53 -0700

    Update Main to Import Fonts

--- code/src/Main.java
+++ code/src/Main.java
@@ -1,9 +1,11 @@
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args){
 
         MyFrame myFrame = new MyFrame(); //create a Frame
+        myFrame.createFonts(); //Create Fonts
         MyJDBC.openConnection(); //connect to Local Database
         Runtime.getRuntime().addShutdownHook(new Thread(MyJDBC::closeConnection)); //close DB connection before exiting
 
