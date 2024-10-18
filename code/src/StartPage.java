1507ed4ef63322c33e45486d9ebf55ee03464fdb
galvare40 <166879908+galvare40@users.noreply.github.com>
2024-10-17 21:43:43 -0700

    Update StartPage.java

--- code/src/StartPage.java
+++ code/src/StartPage.java
@@ -4,15 +4,13 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.Color;
-import javax.swing.Icon;
-import javax.swing.JButton;
+
 
 //WORK ON ATTACHING BUTTONS TO NEXT ACTION AND LOAD INCONS ON JBUTTONS
 
 public class StartPage extends JPanel
 {
   //button variables for navigation bar and texts
- JButton homeButton, forumButton, closetButton, dashboardButton, reviewButton;
  JLabel aboutUs, aboutText, MissonText;
 
  //constructor
@@ -26,10 +24,10 @@ public class StartPage extends JPanel
 
 
 
-   //-------------------CALL NAVIGATION BAR-----------------------------
+   //-------------------CALL NAVIGATION BAR AND SET BOUNDS-----------------------------
         // Add the navigation bar at the bottom
         NavigationBar navigationBar = new NavigationBar();
-        navigationBar.setBounds(0, 720, 500, 60); // Adjust the size and position
+        navigationBar.setBounds(0, 720, 500, 55); // Adjust the size and position
         this.add(navigationBar);
         this.revalidate();
         this.repaint();
@@ -45,14 +43,14 @@ public class StartPage extends JPanel
         this.add(aboutUs); //add it
 
         //create about us TEXT
-        aboutText = new JLabel("<html><div style='width:200px;'>" +
-                                           "Lorem ipsum dolor sit amet, consecteir adipiscing elit," +
-                                           "sed do eisumod tmpora incidndij it " +
-                                           "labore et dolore magna aliqua" + "</div></html>");
+        aboutText = new JLabel("<html><div style='width:230px;'>" +
+                                                "Welcome to Ecouture, A friendly and " +
+                                                 "informative application that will teach you " +
+                                                "about how your shopping habits impact our environment." + "</div></html>");
 
         aboutText.setFont(lato.deriveFont(15f)); //set font and text size
         aboutText.setForeground(new Color (0, 99, 73)); //set color of text
-        aboutText.setBounds(160, 30, 200, 200); //set location
+        aboutText.setBounds(45, 30, 300, 200); //set location
         this.add(aboutText); //add it
 //--------------------------------------------------------------------------------------------------------
 
@@ -60,19 +58,19 @@ public class StartPage extends JPanel
 
  //-----------------------------------------OUR MISSION---------------------------------------------------------------
         //create Our Mission Text
-        MissonText = new JLabel("<html><div style='width:200px;'>" +
-                                            "Our mission is to do blahbalh blah and with that well blah blah" +
-                                            "well blah blah blah blah and lorem ipusm folor sit amet blah " +
-                                            "consexyir adiposicing elir sed do wiusomd temoir incidnt ut balh " +
-                                            " kabire ey dolroe magna aliwue. ullamco alboris nsisi ut by balh " +
-                                            "aliquoid ex ea commaodo consequat. disus aute irrur doleo blah at" + "</div></html>");
+        MissonText = new JLabel("<html><div style='width:250px;'>" +
+                                            "Our mission is to educate the newer generations on sustainable" +
+                                            " shopping methods. With the rise of fast fashion we have seen " +
+                                            "the negative impacts it has had on our environment, from water" +
+                                            " pollution and deforestation to excessive waste. Our mission is " +
+                                            "to convert people to more eco-friendly practices, thereby fostering a healthier planet." + "</div></html>");
 
         MissonText.setFont(lato.deriveFont(15f)); //set font and text size
         MissonText.setForeground(new Color (0, 99, 73)); //set color of text
-        MissonText.setBounds(30, 290, 300, 300); //set location
+        MissonText.setBounds(22, 420, 350, 280); //set location
 
        // Create a custom border with rounded corners
-        Border roundedBorder = new LineBorder(new Color(0, 99, 73), 3, true); // Rounded edge with thickness 3
+        Border roundedBorder = new LineBorder(new Color(0, 99, 73), 5, true); // Rounded edge with thickness 3
        // Add a titled border around the label with the title "OUR MISSION"
         TitledBorder title = new TitledBorder(roundedBorder, "OUR MISSION", TitledBorder.CENTER, TitledBorder.TOP);
         title.setTitleFont(oswald.deriveFont(30f));  // Set custom font size for the title
@@ -84,12 +82,19 @@ public class StartPage extends JPanel
 
 
 //----------------------------------IMAGE FOR HOME PAGE CURRENT IMAGE IS PLACE HOLDER!!!! -----------------------------
-        ImageIcon logo = new ImageIcon (getClass().getResource("pictures/ecouturelogo2.png"));
-        Image scaledLogo = logo.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH); // Resize image to 100x100
-        logo = new ImageIcon(scaledLogo);  // Update logo with resized image
-        JLabel logoDisplay = new JLabel(logo);
-        logoDisplay.setBounds(-20, 10, 200, 200);
-        this.add(logoDisplay);
+// Load the image and scale it to the desired size
+        ImageIcon fact = new ImageIcon(getClass().getResource("pictures/facts.png"));
+        Image scaledFact = fact.getImage().getScaledInstance(300, 249, 4); // Resize image smoothly
+        fact = new ImageIcon(scaledFact);  // Update fact with resized image
+// Create a JLabel to display the image
+        JLabel factDisplay = new JLabel(fact);
+// Set bounds for the image (ensure it fits in the layout)
+        factDisplay.setBounds(50, 150, 300, 300); // Adjust bounds based on the new image size
+// Add the image display to the panel
+        this.add(factDisplay);
+
+
+
 //---------------------------------------------------------------------------------------------------------------------
 
     }
