500dc57b2412e7118cf173c0bb3e9ea777a78d74
JasonTLe <103084397+Jz-on@users.noreply.github.com>
2024-12-11 13:34:49 -0800

    Update userTest.java

--- code/Tests/userTest.java
+++ code/Tests/userTest.java
@@ -1,21 +1,46 @@
     package model;
 
+    import dataAccess.myJDBC;
     import org.junit.Test;
 
     import static org.junit.Assert.*;
+    import model.user;
+
+    import java.sql.Connection;
+    import java.sql.PreparedStatement;
+    import java.sql.ResultSet;
+    import java.sql.SQLException;
 
     public class userTest {
-        private final user user = new user(1, "Jason");
+        private user user;
 
-        @Test
-        public void testGetUserId() {
-            //System.out.println("The user ID is " + user.getUserId());
-            assertEquals(1, user.getUserId());
+        public user getUserID(int userID) throws SQLException {
+            Connection connection = myJDBC.openConnection();
+            //SQL statement that will get user_id and name from the user table where the user is based on user id
+            String sql = "SELECT user_ID, user_realname FROM user WHERE user_ID = ?";
+            //Send the statement to the database
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            //Set the parameter of user_id = ? to userID
+            preparedStatement.setInt(1, userID);
+            //Execute the statement
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                //Stroes the id from user_id column into id
+                int id = rs.getInt("user_id");
+                //stores the real name from the user_realname column into real_name
+                String real_name = rs.getString("user_realname");
+                //returns the new user
+                return new user(id, real_name);
+            }
+            return null;
         }
 
-        @Test
-        public void getRealName() {
-            System.out.println("The name will be Jason");
-            assertEquals("Jason", user.getRealName());
+        @Test //Getting the userId
+        public void testGetUserInformation() throws SQLException {
+            user = getUserID(1);
+            assertNotNull(user);
+            assertEquals(1, user.getUserId());
+            assertEquals("Real name should be Jason", "Jason", user.getRealName());
+            System.out.print("User id is " + user.getUserId() + " and the real name is " + user.getRealName());
         }
     }
